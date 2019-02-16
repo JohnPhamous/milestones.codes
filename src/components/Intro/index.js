0d1a372c3617baba2e28f2c273a9478d4024c31f
John Pham <johnphammail@gmail.com>
2019-02-16 15:25:01 -0800

    updating intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -7,7 +7,7 @@ const Intro = () => (
       <strong>Milestones</strong> is designed to help you keep track of your
       progress learning how to program. The best way to use{" "}
       <strong>Milestones</strong> is to take it every few months and see the
-      increase in what you've learned!
+      progress you've made.
     </p>
   </header>
 );
